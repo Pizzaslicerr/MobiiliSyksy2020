008cc705f705c318735f56a704a0d1fba8be904a
villeheiskanen <64367213+villeheiskanen@users.noreply.github.com>
2020-09-23 13:08:24 +0300

    VilleCommit3 23.9.
    Minor endscene fixings

---
+++ MobiiliSyksy2020/Assets/LevelComplete.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelComplete : MonoBehaviour
+{
+    public GameObject Endscrn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Fox")
+        {
+            Endscrn.gameObject.SetActive(true);
+        }
+    }
+}
