34c6ccac0be054f843c408a27ac97f23be3b2c78
Mikko Kyllönen <mikkokyllonen1@kamk.fi>
2020-09-23 13:04:34 +0300

    Added edge limits to map screen (needs smoothing)

--- MobiiliSyksy2020/Assets/Scripts/UI/MapScroll.cs
+++ MobiiliSyksy2020/Assets/Scripts/UI/MapScroll.cs
@@ -12,16 +12,19 @@ public class MapScroll : MonoBehaviour, IDragHandler, IEndDragHandler
         XY
     };
 
-    [SerializeField]
-    private coordinateDirections scrollDirection = coordinateDirections.X;
+    [SerializeField] private coordinateDirections scrollDirection = coordinateDirections.X; //is overridden, but is here just to get rid of warning messages in the debug log
+    [SerializeField] private GameObject mapScreen;
+    private RectTransform mapScreenRect;
 
     private float difference;
     private Vector2 differenceXY;
     private Vector3 panelPosition;
+    private Vector3 endPosition;
 
     private void Start()
     {
         panelPosition = transform.position;
+        mapScreenRect = mapScreen.GetComponent<RectTransform>();
     }
     public void OnDrag(PointerEventData data)
     {
@@ -30,12 +33,27 @@ public class MapScroll : MonoBehaviour, IDragHandler, IEndDragHandler
             case coordinateDirections.X:
                 {
                     difference = data.pressPosition.x - data.position.x;
+                    endPosition = panelPosition - new Vector3(difference, 0, 0);
                     transform.position = panelPosition - new Vector3(difference, 0, 0);
                     break;
                 }
             case coordinateDirections.Y:
                 {
                     difference = data.pressPosition.y - data.position.y;
+
+                    if (transform.position.y > 0)
+                    {
+                        endPosition = new Vector3(transform.position.x, 0, 0);
+                    }
+                    else if (transform.position.y < (mapScreenRect.rect.height - Screen.height) * -1)
+                    {
+                        endPosition = new Vector3(transform.position.x, (mapScreenRect.rect.height - Screen.height) * -1, 0);
+                    }
+                    else
+                    {
+                        endPosition = panelPosition - new Vector3(0, difference, 0);
+                    }
+
                     transform.position = panelPosition - new Vector3(0, difference, 0);
                     break;
                 }
@@ -50,8 +68,10 @@ public class MapScroll : MonoBehaviour, IDragHandler, IEndDragHandler
 
     public void OnEndDrag(PointerEventData data)
     {
+        transform.position = endPosition;
         panelPosition = transform.position;
 
+        endPosition = Vector3.zero;
         difference = 0f;            //probably not necessary, but here just in case
         differenceXY = Vector2.zero;
     }
