bcc088cd3aa8d25feca3c47f34ce349a545ffdbb
Mikko Kyllönen <mikkokyllonen1@kamk.fi>
2020-09-24 15:20:40 +0300

    Map screen and level loading now works + more
    Made it easier to load levels by allowing developers to simply drag and drop the scene asset into the script for loading

--- MobiiliSyksy2020/Assets/Scripts/Scene Management/InitializeSceneSwitch.cs
+++ MobiiliSyksy2020/Assets/Scripts/Scene Management/InitializeSceneSwitch.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class InitializeSceneSwitch : MonoBehaviour
+namespace Utilities
 {
-    [SerializeField] private LoadingScreens loadScreenType = LoadingScreens.Leaves;
-    public void ChangeScenes(int sceneToLoad)
+    public class InitializeSceneSwitch : MonoBehaviour
     {
-        SceneHandler.instance.SceneLoad(sceneToLoad, this.gameObject.scene.buildIndex, loadScreenType);
+        [SerializeField] private LoadingScreens loadScreenType = LoadingScreens.Leaves;
+        [SerializeField] private SceneField sceneToLoad;
+        [Tooltip("Whether or not the previous scene should be unloaded. For most cases, leave this enabled.")]
+        [SerializeField] private bool unloadPreviousScene = true;
+
+        public void ChangeScenes()
+        {
+            if (unloadPreviousScene)
+            {
+                SceneHandler.instance.SceneLoad(sceneToLoad, this.gameObject.scene.buildIndex, loadScreenType);
+            }
+            else
+            {
+                SceneHandler.instance.SceneLoad(sceneToLoad, loadScreenType);
+            }
+        }
     }
 }
--- MobiiliSyksy2020/Assets/Scripts/Scene Management/SceneHandler.cs
+++ MobiiliSyksy2020/Assets/Scripts/Scene Management/SceneHandler.cs
@@ -6,69 +6,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneHandler : MonoBehaviour
+namespace Utilities
 {
-    private LoadingScreenManager loadingScreenManager;  //loading screens are only visible (for now) with LoadAndUnloadScene()
-    private int loadedScene;
-    public int LoadedScene { set => loadedScene = value; }
-
-
-    public static SceneHandler instance;
-    private void Awake()
+    public class SceneHandler : MonoBehaviour
     {
-        instance = this;    //public singleton, so easily accessible
-        loadingScreenManager = gameObject.GetComponent<LoadingScreenManager>();
-    }
-
-    //scene load operations are added to this list; this is required for loading bars and especially to make sure scenes have been loaded
-    //two voids for method overloads, having a scene to unload is not necessary
-    List<AsyncOperation> operations = new List<AsyncOperation>();
-    public void SceneLoad(int sceneToLoad, LoadingScreens LoadingScreen)
-    {
-        operations.Add(SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive));
-
-        StartCoroutine(GetSceneLoadProgress());
-    }
+        private LoadingScreenManager loadingScreenManager;  //loading screens are only visible (for now) with LoadAndUnloadScene()
+        private int loadedScene;
+        public int LoadedScene { set => loadedScene = value; }
 
 
-    public void SceneLoad(int sceneToLoad, int sceneToUnload, LoadingScreens LoadScreenType)
-    {
-        loadingScreenManager.Activate((int)LoadScreenType);    //This is temporary, as a selector should be added later
-        operations.Add(SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive));
-        operations.Add(SceneManager.UnloadSceneAsync(sceneToUnload));
-
-        StartCoroutine(GetSceneLoadProgress((int)LoadScreenType));
-    }
-
-
-    private IEnumerator GetSceneLoadProgress()
-    {
-        for (int i = 0; i < operations.Count; i++)
+        public static SceneHandler instance;
+        private void Awake()
         {
-            while (!operations[i].isDone)   //runs until the scene the AsyncOperation list is handling has loaded, then moves on to the next
-            {
-                yield return null;
-            }
+            instance = this;    //public singleton, so easily accessible
+            loadingScreenManager = gameObject.GetComponent<LoadingScreenManager>();
         }
 
-        operations.Clear();
-        yield break;
-    }
-
-
-    private IEnumerator GetSceneLoadProgress(int loadScreenVariant)
-    {
-        for (int i = 0; i < operations.Count; i++)
+        //scene load operations are added to this list; this is required for loading bars and especially to make sure scenes have been loaded
+        //two voids for method overloads, having a scene to unload is not necessary
+        List<AsyncOperation> operations = new List<AsyncOperation>();
+        public void SceneLoad(SceneField sceneToLoad, LoadingScreens LoadingScreen)
         {
-            while (!operations[i].isDone)
-            {
-                yield return null;
-            }
-        }
-        yield return new WaitForSecondsRealtime(1f);
+            operations.Add(SceneManager.LoadSceneAsync(sceneToLoad.SceneName, LoadSceneMode.Additive));
 
-        loadingScreenManager.Deactivate(loadScreenVariant);
-        operations.Clear();
-        yield break;
+            StartCoroutine(GetSceneLoadProgress());
+        }
+
+
+        public void SceneLoad(SceneField sceneToLoad, int sceneToUnload, LoadingScreens LoadScreenType)
+        {
+            loadingScreenManager.Activate((int)LoadScreenType);
+            operations.Add(SceneManager.LoadSceneAsync(sceneToLoad.SceneName, LoadSceneMode.Additive));
+            operations.Add(SceneManager.UnloadSceneAsync(sceneToUnload));
+
+            StartCoroutine(GetSceneLoadProgress((int)LoadScreenType));
+        }
+
+
+        private IEnumerator GetSceneLoadProgress()
+        {
+            for (int i = 0; i < operations.Count; i++)
+            {
+                while (!operations[i].isDone)   //runs until the scene the AsyncOperation list is handling has loaded, then moves on to the next
+                {
+                    yield return null;
+                }
+            }
+
+            operations.Clear();
+            yield break;
+        }
+
+
+        private IEnumerator GetSceneLoadProgress(int loadScreenVariant)
+        {
+            for (int i = 0; i < operations.Count; i++)
+            {
+                while (!operations[i].isDone)
+                {
+                    yield return null;
+                }
+            }
+            yield return new WaitForSecondsRealtime(1f);
+
+            loadingScreenManager.Deactivate(loadScreenVariant);
+            operations.Clear();
+            yield break;
+        }
     }
 }
