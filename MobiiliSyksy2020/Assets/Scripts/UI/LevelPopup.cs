6d41f01935792df4a0c0998c7184ae1dbb732f4f
Mikko Kyllönen <mikkokyllonen1@kamk.fi>
2020-11-13 14:26:45 +0200

    Added janky system for displaying apples (saving yet to come)

--- MobiiliSyksy2020/Assets/Scripts/UI/LevelPopup.cs
+++ MobiiliSyksy2020/Assets/Scripts/UI/LevelPopup.cs
@@ -21,9 +21,9 @@ public class LevelPopup : MonoBehaviour
 
     private void Start()
     {
-        Debug.LogError("Warning Mikko that this still needs to be fixed tomorrow");
         //Gets the apple count of the level corresponding to the sibling index.
-        appleCount = MapData.instance.Data.LevelApples[transform.parent.transform.GetSiblingIndex()];
+        appleCount = MapData.instance.Data.LevelApples[transform.parent.parent.transform.GetSiblingIndex()];
+        Debug.Log(transform.parent.transform.GetSiblingIndex());
         for (int i = 0; i < appleCount; i++)
         {
             apples[i].SetActive(true);
