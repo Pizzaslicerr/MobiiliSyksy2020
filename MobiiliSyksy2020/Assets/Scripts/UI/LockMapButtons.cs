542a706aea63e40b4ffc3ff5722210578e961d52
Mikko Kyllönen <mikkokyllonen1@kamk.fi>
2020-12-04 12:19:08 +0200

    Made buttons hide themselves if still locked
    Also turned level buttons 1-5 into prefabs

---
+++ MobiiliSyksy2020/Assets/Scripts/UI/LockMapButtons.cs
@@ -0,0 +1,24 @@
+﻿//LockMapButtons.cs by Mikko Kyllönen
+//Hides the level buttons that have not been unlocked yet when map screen loads.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class LockMapButtons : MonoBehaviour
+    {
+        private InitializeSceneSwitch init;
+
+        private void Start()
+        {
+            init = GetComponent<InitializeSceneSwitch>();
+
+            if (init.LevelNumber - 1 > SaveManager.instance.SaveData.LatestCompletedLevel)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
