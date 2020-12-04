4ea1f18709db9945f87ce9b8f95ada15a92d71ba
Mikko Kyllönen <mikkokyllonen1@kamk.fi>
2020-12-04 15:36:49 +0200

    Rearranged folders, added credits menu, made red buttons work

---
+++ MobiiliSyksy2020/Assets/Scripts/UI/Visibility/ChangeMenuVisibility.cs
@@ -0,0 +1,19 @@
+﻿//ChangeMenuVisibility.cs by Mikko Kyllönen
+//Shows and/or hides specified UI elements.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenuVisibility : MonoBehaviour
+{
+    public void ShowUIElement(GameObject element)
+    {
+        element.SetActive(true);
+    }
+
+    public void HideUIElement(GameObject element)
+    {
+        element.SetActive(false);
+    }
+}
